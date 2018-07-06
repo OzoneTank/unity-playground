58a213f8cea89ff23496a92285161eab17c7ea8c
Ed Cole (OzoneTank) <ed.cole@smashingboxes.com>
2018-07-06 16:27:05 -0400

    random seed example

---
+++ Assets/Scripts/RandomSeed/SeedTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SeedTest : MonoBehaviour {
+    
+    public string seed = "";
+    public float min;
+    public float max;
+    private float clicks = 0;
+
+    public TextMeshPro boxText;
+    public TextMeshProUGUI clickText;
+
+
+	void Start () {
+        ChangeSeed(seed);
+	}
+
+    private void OnMouseDown()
+    {
+        clicks++;
+        float num = Random.Range(min, max);
+        boxText.text = "" + num;
+        clickText.text = "Clicks: " + clicks;
+    }
+
+    public void ChangeSeed(string newSeed) {
+        seed = newSeed;
+        clicks = 0;
+        Random.InitState(seed.GetHashCode());
+        boxText.text = "Click Me!";
+        clickText.text = "Clicks: " + clicks;
+    }
+}
