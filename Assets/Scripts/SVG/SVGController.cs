f837a7bbd36017a80e1b31a7dc0716ae31191c12
Ed Cole (OzoneTank) <ed.cole@smashingboxes.com>
2018-07-12 19:53:14 -0400

    SVG importer

---
+++ Assets/Scripts/SVG/SVGController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SVGController : MonoBehaviour {
+
+    public TextMeshProUGUI valueText;
+
+    public void SetZoom(float value) {
+        gameObject.transform.localScale = Vector3.one * value;
+        valueText.text = value.ToString("F2");
+    }
+}
